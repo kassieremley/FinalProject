6364f849f1a0af36f2f80ebf39b148ec9cfda5b7
Frick12 <harpodouglas@gmail.com>
2017-03-07 14:39:35 -0600

    new code

--- src/finalProject/FirstClass.java
+++ src/finalProject/FirstClass.java
@@ -2,19 +2,27 @@
  * 
  */
 package finalProject;
-
 /**
  * @author KR131457
  *
  */
-public class FirstClass {
-
+import java.awt.Dimension;
+import javax.swing.JFrame;
+public class FirstClass 
+{
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) 
+	{
+		JFrame frame = new JFrame("Snake");
+		frame.setContentPane(new GamePanel());
+		frame.pack();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setResizable(false);
+		frame.setPreferredSize(new Dimension(GamePanel.WIDTH, GamePanel.HEIGHT));
+		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
 	}
 
 }
